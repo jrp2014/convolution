5bf1d2592bd78b9e55e9e973d421e44eb2930c5b
jrp2014 <jrp@dial.pipex.com>
2019-02-17 14:27:21 +0000

    Make Stream Comonadic

--- research/Stream.hs
+++ research/Stream.hs
@@ -2,19 +2,35 @@ module Stream where
 
 -- https://www.cs.ox.ac.uk/ralf.hinze/publications/CSC.pdf
 --
-import qualified Data.Stream                   as S
-import           Data.Stream                    ( Stream(..)
-                                                , (<:>)
-                                                )
+import qualified Data.Stream as S
+import Data.Stream (Stream(..), (<:>))
+
+import Control.Comonad
 
 instance (Num a) => Num (Stream a) where
-   (+)          =  S.zipWith (+)
-   (-)          =  S.zipWith (-)
-   (*)          =  S.zipWith (*)
-   negate       =  S.map negate
-   abs          =  S.map abs
-   signum       =  S.map signum
-   fromInteger  =  S.repeat . fromInteger
+  (+) = S.zipWith (+)
+  (-) = S.zipWith (-)
+  (*) = S.zipWith (*)
+  negate = S.map negate
+  abs = S.map abs
+  signum = S.map signum
+  fromInteger = S.repeat . fromInteger
+
+instance Comonad Stream where
+  extract = S.head
+  extend f s = f s <:> extend f (S.tail s)
+  duplicate s = s <:> duplicate (S.tail s)
+
+sumS :: Num a => Int -> Stream a -> a
+sumS n s
+  | n <= 0 = 0
+  | otherwise = S.head s + sumS (n - 1) (S.tail s)
+
+average :: Fractional a => Int -> Stream a -> a
+average n stm = sumS n stm / fromIntegral n
+
+movingAvg :: Fractional a => Int -> Stream a -> Stream a
+movingAvg n = extend (average n)
 
 convolve :: Num a => Stream a -> Stream a -> Stream a
 convolve s t =
